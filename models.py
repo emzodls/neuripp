5ab4c19f6b7faff8583f15f1b79a911cb4d35446
Emzo de los Santos <emzodls@gmail.com>
2019-03-25 22:52:51 +0000

    added license TO DO: allow command line to run

--- models.py
+++ models.py
@@ -1,3 +1,26 @@
+# Copyright (C) 2019 Emmanuel LC. de los Santos
+# University of Warwick
+# Warwick Integrative Synthetic Biology Centre
+#
+# License: GNU Affero General Public License v3 or later
+# A copy of GNU AGPL v3 should have been included in this software package in LICENSE.txt.
+'''
+    This file is part of NeuRiPP.
+
+    NeuRiPP is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    NeuRiPP is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with NeuRiPP.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.models import Sequential
--- train.py
+++ train.py
@@ -1,10 +1,31 @@
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.models import Sequential
+# Copyright (C) 2019 Emmanuel LC. de los Santos
+# University of Warwick
+# Warwick Integrative Synthetic Biology Centre
+#
+# License: GNU Affero General Public License v3 or later
+# A copy of GNU AGPL v3 should have been included in this software package in LICENSE.txt.
+'''
+    This file is part of NeuRiPP.
+
+    NeuRiPP is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    NeuRiPP is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with NeuRiPP.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+
 from utils import *
 from random import shuffle
 from models import create_model_lstm,create_model_conv_lstm,create_model_conv,create_model_conv_parallel,create_model_conv_parallel_lstm
-import os
+
 
 
 def mix_samples(set_a,set_b,set_a_frac=0.5,set_b_frac=0.5):
